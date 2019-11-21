9879cbb92ebe62ba9ed19a55a68034507f5586de
Nathan <nathan.froeh@gmail.com>
2019-11-21 14:18:32 -0700

    Add view state

--- src/Hotel/Hotel.jsx
+++ src/Hotel/Hotel.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 
 export class Hotel extends Component {
+  constructor() {
+    super()
+    this.state={
+      view: 'description'
+    }
+  }
   render() {
+    console.log(this.props.currentHotel)
     return (
       <div>
         
