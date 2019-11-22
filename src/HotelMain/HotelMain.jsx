480072d54c0f83fdcb702162bdb8b211ccd9264f
Nathan <nathan.froeh@gmail.com>
2019-11-22 12:55:04 -0700

    Add main page prop text

--- src/HotelMain/HotelMain.jsx
+++ src/HotelMain/HotelMain.jsx
@@ -3,7 +3,19 @@ import React from 'react'
 export default function HotelMain({hotel}) {
   return (
     <main>
-      <h1>{hotel.name}</h1>
+      <div>
+        <h1>{hotel.name}</h1>
+        <p>stars</p>
+        <div>
+          <p>location</p>
+          <p>phone</p>
+          <p>message</p>
+        </div>
+        <p>
+          <strong>price</strong>
+          HOTEL ROOMS FROM
+        </p>
+      </div>
     </main>
   )
 }
