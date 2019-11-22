b2cde5b1d43f3dfc6429ba31516d9e6b211a11b4
Nathan <nathan.froeh@gmail.com>
2019-11-22 12:57:28 -0700

    Set HotelMain to class component

--- src/HotelMain/HotelMain.jsx
+++ src/HotelMain/HotelMain.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-export default function HotelMain({hotel}) {
-  return (
-    <main>
-      <div>
-        <h1>{hotel.name}</h1>
-        <p>stars</p>
+class HotelMain extends Component {
+  constructor() {
+    super()
+    this.state={
+      view: 'description'
+    }
+  }
+
+  render() {
+    const {name} = this.props.hotel;
+    return (
+      <main>
         <div>
-          <p>location</p>
-          <p>phone</p>
-          <p>message</p>
+          <h1>{name}</h1>
+          <p>stars</p>
+          <div>
+            <p>location</p>
+            <p>phone</p>
+            <p>message</p>
+          </div>
+          <p>
+            <strong>price</strong>
+            HOTEL ROOMS FROM
+          </p>
         </div>
-        <p>
-          <strong>price</strong>
-          HOTEL ROOMS FROM
-        </p>
-      </div>
-    </main>
-  )
+      </main>
+    )
+  }
 }
