72059824fce2fe9904fef14cbd9db2a51a68749c
Nathan <nathan.froeh@gmail.com>
2019-11-22 17:57:14 -0700

    Sort hotels

--- src/HotelSide/HotelSide.jsx
+++ src/HotelSide/HotelSide.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import '../utils/icons.svg';
 import './HotelSide.scss';
+import {getHotelList} from '../utils/apiCalls';
 
 class HotelSide extends Component {
   constructor() {
@@ -8,6 +9,38 @@ class HotelSide extends Component {
     this.state={
       hotelList: {}
     }
+  }
+
+  componentDidMount() {
+    this.setHotelList()
+  }
+
+  setHotelList = async () => {
+    const hotels = await getHotelList()
+    const uniqueHotels = this.filterHotels(hotels)
+    const sorted = this.sortHotels(uniqueHotels)
+    this.setState({hotelList: sorted})
+  }
+
+  filterHotels = (hotels) => {
+    return hotels.list.reduce((acc, hotel) => {
+      const boolean = acc.find(obj => obj.name === hotel.name)
+      if(!boolean) acc.push(hotel)
+      return acc
+    }, [])
+  }
+
+  sortHotels = (hotels) => {
+    return hotels.sort((a, b) => {
+      const nameA = a.name.toUpperCase()
+      const nameB = b.name.toUpperCase()
+      if (nameA < nameB) {
+        return -1;
+      } else if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    })
   }
 
   render() {
