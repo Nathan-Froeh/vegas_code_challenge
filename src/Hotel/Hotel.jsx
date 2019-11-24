58dbba85b8089f9d1b1c74a1b4121f3f52fb0a0b
Nathan <nathan.froeh@gmail.com>
2019-11-24 10:06:23 -0700

    Prettier Hotel/HotelSide

--- src/Hotel/Hotel.jsx
+++ src/Hotel/Hotel.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import HotelSide from '../HotelSide/HotelSide';
-import HotelMain from '../HotelMain/HotelMain';
-import './Hotel.scss';
+import React from "react";
+import HotelSide from "../HotelSide/HotelSide";
+import HotelMain from "../HotelMain/HotelMain";
+import "./Hotel.scss";
 
-export default function Hotel({currentHotel}) {
+export default function Hotel({ currentHotel }) {
   return (
-    <div className='Hotel'>
-      <HotelSide productImage={currentHotel.media}/>
-      <HotelMain hotel={currentHotel}/>
+    <div className="Hotel">
+      <HotelSide productImage={currentHotel.media} />
+      <HotelMain hotel={currentHotel} />
     </div>
-  )
+  );
 }
